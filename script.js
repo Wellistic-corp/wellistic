b6a9a754eba9a08bf74e51a34b849133f1c1d99f
Wellistic-corp <154348772+Wellistic-corp@users.noreply.github.com>
2024-01-12 18:00:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
+
+                      const miBoton= document.getElementById('miBoton');
+let boton = document.getElementById("miBoton");
